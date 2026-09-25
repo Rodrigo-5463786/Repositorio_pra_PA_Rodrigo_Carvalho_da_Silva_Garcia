703d7116a9e804cd3571e9b1fb246024d53b3de5
Rodrigo-5463786 <zacsalvatore3@gmail.com>
2026-09-25 11:30:10 -0300

    Delete exercicio_27.py

--- exercicio_27.py
+++
@@ -1,8 +0,0 @@
-while True:
-    nota = float(input("Digite uma nota de 0 a 10: "))
-
-    if 0 <= nota <= 10:
-        print("Nota válida!")
-        break
-    else:
-        print("Nota inválida! Digite novamente.")
