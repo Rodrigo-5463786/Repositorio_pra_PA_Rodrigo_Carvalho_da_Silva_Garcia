40bd18ffb141f10f2deada5c5d169497de07692d
Rodrigo-5463786 <zacsalvatore3@gmail.com>
2026-09-25 11:32:09 -0300

    Delete exercicio_06.py

--- exercicio_06.py
+++
@@ -1,7 +0,0 @@
-valor = float(input("Digite o valor original do produto: "))
-
-desconto = valor * 0.15
-valor_final = valor - desconto
-
-print("Valor do desconto:", desconto)
-print("Valor com desconto:", valor_final)
