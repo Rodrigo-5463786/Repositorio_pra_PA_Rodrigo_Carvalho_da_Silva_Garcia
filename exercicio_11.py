5e95fa35e0b7d51a6631cf1fa74d1f73eaf77351
Rodrigo-5463786 <zacsalvatore3@gmail.com>
2026-09-25 11:32:48 -0300

    Delete exercicio_11.py

--- exercicio_11.py
+++
@@ -1,9 +0,0 @@
-numero1 = float(input("Digite o primeiro número: "))
-numero2 = float(input("Digite o segundo número: "))
-
-if numero1 > numero2:
-    print("O maior número é:", numero1)
-elif numero2 > numero1:
-    print("O maior número é:", numero2)
-else:
-    print("Os dois números são iguais.")
