7ed8a2a68a0f2441ce2f06e085779bf7116ef864
Rodrigo-5463786 <zacsalvatore3@gmail.com>
2026-09-25 11:27:07 -0300

    Delete exercicio_20.py

--- exercicio_20.py
+++
@@ -1,9 +0,0 @@
-senha_correta = "etec123"
-
-senha = input("Digite a senha: ")
-
-while senha != senha_correta:
-    print("Senha incorreta!")
-    senha = input("Digite a senha novamente: ")
-
-print("Acesso Permitido.")
